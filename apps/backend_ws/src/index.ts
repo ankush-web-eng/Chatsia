226b469fd167f7c458e523b530a53969123a64ac
Ankush <deshwalankush23@gmail.com>
2024-07-07 19:08:09 +0530

    Vite React updated (added)

---
+++ apps/backend_ws/src/index.ts
@@ -0,0 +1,31 @@
+import WebSocket, { WebSocketServer } from "ws";
+import http from "http";
+
+const server = http.createServer((req, res) => {
+    if (req.url !== "/favicon.ico") {
+        console.log(new Date() + " Received request for " + req.url);
+        res.end("Hi there!");
+    }
+});
+
+const wss = new WebSocketServer({ server });
+
+let users = 1
+wss.on("connection", (ws) => {
+    ws.on("error", error => console.error(error))
+
+    ws.on("message", (data, isBinary) => {
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+                console.log(data.toString())
+            }
+        })
+    })
+    console.log("Users : " + users++)
+    ws.send("Hello! Message From Server!!")
+})
+
+server.listen(3001, () => {
+    console.log("Server started on http://localhost:3001");
+})
